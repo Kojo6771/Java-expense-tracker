b92bc422c46302a49d2130c4130eba88f142ecdf
Kojo6771 <230165858@aston.ac.uk>
2026-08-26 03:45:16 +0100

    Update ExpenseCategory.java
    I have added the set method for the expense category

--- expense-tracker/src/main/java/com/kojo/ExpenseCategory.java
+++ expense-tracker/src/main/java/com/kojo/ExpenseCategory.java
@@ -4,18 +4,21 @@ package com.kojo;
 public class ExpenseCategory {
     private String name;
 
+
+    //ExpenseCategory constructor
     public ExpenseCategory(Sring name){
         this.name = name;
     }
     
+    //ExpenseCategory get method
     public String getName(){
         return name;
     }
 
-
-
-
-
+    //ExpenseCategory set method
+    public void setName(String name){
+        this.name = name;
+    }
 
 
 
