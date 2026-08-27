e30e8515ec79060979997b7baade783a1d471387
Kojo6771 <230165858@aston.ac.uk>
2026-08-27 03:39:31 +0100

    Created the expense Manager class *not finished*
    I have created the expense manager class which allows expenses to be added to an array list and stored. I have made the add expense method and the get all expenses method

--- expense-tracker/src/main/java/com/kojo/ExpenseManager.java
+++ expense-tracker/src/main/java/com/kojo/ExpenseManager.java
@@ -1,5 +1,36 @@
 package com.kojo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ExpenseManager {
+    //Expenses array list field
+    private final List<Expense> expenses;
     
+
+    //Constructor of the ExpenseManager
+    public ExpenseManager(){
+        expenses = new ArrayList<Expense>();
+
+
+    }
+
+    //Adds an expense to the list
+    public void addExpense(Expense expense){
+        if(expense != null){
+            expenses.add(expense);
+        }
+            
+    }
+
+
+    //Returns a copy of the expenses list
+    public List<Expense> getAllExpenses(){
+        return List.copyOf(expenses);
+    }
+
+
+
 }
+
+
